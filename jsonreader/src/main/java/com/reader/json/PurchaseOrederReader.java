c3e4a921b806686236c05e8b78f1ad044de596b9
mahesh <mahesh@gmail.com>
2019-03-28 22:44:39 +0530

    removed unwanted import files

--- jsonreader/src/main/java/com/reader/json/PurchaseOrederReader.java
+++ jsonreader/src/main/java/com/reader/json/PurchaseOrederReader.java
@@ -4,13 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
-
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import javax.json.JsonWriter;
 
 public class PurchaseOrederReader {
 
